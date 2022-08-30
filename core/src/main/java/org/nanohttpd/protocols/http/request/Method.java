b5f80b39602b822f7bc0b9f00856e6a8960b150c
Eli Bishop <eli@launchdarkly.com>
2022-08-30 16:25:42 -0700

    allow any HTTP method

--- core/src/main/java/org/nanohttpd/protocols/http/request/Method.java
+++ core/src/main/java/org/nanohttpd/protocols/http/request/Method.java
@@ -1,5 +1,8 @@
 package org.nanohttpd.protocols.http.request;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * #%L
  * NanoHttpd-Core
@@ -34,38 +37,82 @@ package org.nanohttpd.protocols.http.request;
  */
 
 /**
- * HTTP Request methods, with the ability to decode a <code>String</code> back
- * to its enum value.
+ * Represents an HTTP request method/verb. This class includes predefined instances for commonly
+ * used verbs, but it is not an enum, because HTTP allows other verbs to be used as long as the
+ * client and server both agree they are valid.
  */
-public enum Method {
-    GET,
-    PUT,
-    POST,
-    DELETE,
-    HEAD,
-    OPTIONS,
-    TRACE,
-    CONNECT,
-    PATCH,
-    PROPFIND,
-    PROPPATCH,
-    MKCOL,
-    MOVE,
-    COPY,
-    LOCK,
-    UNLOCK,
-    NOTIFY,
-    SUBSCRIBE;
+public final class Method {
+    private String name;
+    
+    private Method(String name) {
+      this.name = name;
+    }
+    
+    private static Map<String, Method> BUILTINS = new HashMap<>();
+    private static Method makeBuiltin(String name) {
+        Method m = new Method(name);
+        BUILTINS.put(name, m);
+        return m;
+    }
+    
+    public static Method
+        GET = makeBuiltin("GET"),
+        PUT = makeBuiltin("PUT"),
+        POST = makeBuiltin("POST"),
+        DELETE = makeBuiltin("DELETE"),
+        HEAD = makeBuiltin("HEAD"),
+        OPTIONS = makeBuiltin("OPTIONS"),
+        TRACE = makeBuiltin("TRACE"),
+        CONNECT = makeBuiltin("CONNECT"),
+        PATCH = makeBuiltin("PATCH"),
+        PROPFIND = makeBuiltin("PROPFIND"),
+        PROPPATCH = makeBuiltin("PROPPATCH"),
+        MKCOL = makeBuiltin("MKCOL"),
+        MOVE = makeBuiltin("MOVE"),
+        LOCK = makeBuiltin("LOCK"),
+        UNLOCK = makeBuiltin("UNLOCK"),
+        NOTIFY = makeBuiltin("NOTIFY"),
+        SUBSCRIBE = makeBuiltin("SUBSCRIBE");
 
-    public static Method lookup(String method) {
-        if (method == null)
-            return null;
-
-        try {
-            return valueOf(method);
-        } catch (IllegalArgumentException e) {
-            // TODO: Log it?
+    /**
+     * Returns a Method instance for the given name, as long as it is syntactically valid. 
+     * @param name the method name as a string
+     * @return a Method, or null if not valid
+     */
+    public static Method lookup(String name) {
+        if (name == null) {
             return null;
         }
+        Method m = BUILTINS.get(name);
+        if (m != null) {
+          return m;
+        }
+        return isValid(name) ? new Method(name) : null;
+    }
+    
+    private static boolean isValid(String name) {
+        // allowable character set is the same as for any "token" in HTTP: no control chars or separators
+        for (int i = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+            if (ch <= ' ' || "()<>@,;:\\\"/[]?={}".contains(String.valueOf(ch))) {
+              return false;
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+      return name;
+    }
+    
+    @Override
+    public int hashCode() {
+      return name.hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+      return other instanceof Method && ((Method)other).name.equals(name);
     }
 }
